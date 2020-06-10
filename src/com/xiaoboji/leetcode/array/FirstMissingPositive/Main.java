cd530d354d22a8c47571124a70db0c783f726992
xiaoboji <xiaoboji1990@gmail.com>
2020-06-10 23:49:46 +0800

    [Array][缺失的第一个正数][FirstGoodSolution][数据放到他本来应该在的位置，即nums[i]=i+1]

--- src/com/xiaoboji/leetcode/array/FirstMissingPositive/Main.java
+++ src/com/xiaoboji/leetcode/array/FirstMissingPositive/Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static int[] testcase = new int[]{1,1};
 
     public static void main(String[] args) {
-        IFirstMissingPositive firstMissingPositive =  new MySolution();
+        IFirstMissingPositive firstMissingPositive =  new FirstGoodSolution();
         System.out.println(firstMissingPositive.firstMissingPositive(testcase));
     }
 
