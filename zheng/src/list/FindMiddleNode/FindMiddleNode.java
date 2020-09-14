a6a6722c412b622f748741f87ec7eaf354c15403
xiaoboji <xiaoboji1990@gmail.com>
2020-09-15 01:01:25 +0800

    [xiaoboji][zheng][create][FindMiddleNode]

---
+++ zheng/src/list/FindMiddleNode/FindMiddleNode.java
@@ -0,0 +1,56 @@
+package list.FindMiddleNode;
+
+/**
+ * @program: ds-algo
+ * @description: 实现求链表的中间结点
+ * @author: xiaoboji
+ * @create: 2020-09-15 00:49
+ */
+public class FindMiddleNode {
+  public static void main(String[] args) {
+    ListNode l11111 = new ListNode(5);
+
+    ListNode l1111 = new ListNode(4, l11111);
+    ListNode l111 = new ListNode(3, l1111);
+    ListNode l11 = new ListNode(2, l111);
+    ListNode l1 = new ListNode(1, l11);
+
+    System.out.println(solution(l1));
+  }
+
+  public static int solution(ListNode listNode) {
+    if (listNode.next == null || listNode == null) {
+      return listNode.val;
+    }
+    ListNode first = listNode;
+    ListNode second = listNode;
+
+    while (second != null && second.next != null) {
+      first = first.next;
+      second = second.next.next;
+    }
+
+    return first.val;
+  }
+}
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+
+  @Override
+  public String toString() {
+    return "ListNode{" + "val=" + val + ", next=" + next + '}';
+  }
+}
