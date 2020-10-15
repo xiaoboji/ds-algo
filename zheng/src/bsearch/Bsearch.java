8a87a555f901270650ea5e4562cf8dac5d344855
xiaoboji <xiaoboji1990@gmail.com>
2020-10-15 23:32:40 +0800

    [xiaoboji][zheng][update][Bsearch]

---
+++ zheng/src/bsearch/Bsearch.java
@@ -0,0 +1,15 @@
+package bsearch;
+
+/**
+ * @program: ds-algo
+ * @description: 实现一个有序数组的二分查找算法
+ * @author: xiaoboji
+ * @create: 2020-10-14 23:50
+ */
+public class Bsearch {
+  public static void main(String[] args) {
+    int[] a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  }
+
+  public void bsearch(int[] a, int n, int value) {}
+}
