b0cb2782d7b89e1b1d4583ba19bcbf25f569b463
xiaoboji <xiaoboji1990@gmail.com>
2020-10-15 23:54:21 +0800

    [xiaoboji][zheng][update][Bsearch]

--- zheng/src/bsearch/Bsearch.java
+++ zheng/src/bsearch/Bsearch.java
@@ -9,7 +9,24 @@ package bsearch;
 public class Bsearch {
   public static void main(String[] args) {
     int[] a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    System.out.println(bsearch(a, a.length, 8));
   }
 
-  public void bsearch(int[] a, int n, int value) {}
+  public static int bsearch(int[] a, int n, int value) {
+    int low = 0;
+    int high = n - 1;
+    while (high >= low) {
+      int mid = (low + high) / 2;
+      if (value == a[mid]) {
+        return mid;
+      }
+      if (value > a[mid]) {
+        low = mid + 1;
+      }
+      if (value < a[mid]) {
+        high = mid - 1;
+      }
+    }
+    return -1;
+  }
 }
