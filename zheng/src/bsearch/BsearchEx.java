ed84fd255ce31de80e5189092418ccea408c4c26
xiaoboji <xiaoboji1990@gmail.com>
2020-10-18 00:09:51 +0800

    [xiaoboji][zheng][add][BsearchEx]

--- zheng/src/bsearch/BsearchEx.java
+++ zheng/src/bsearch/BsearchEx.java
@@ -18,9 +18,9 @@ public class BsearchEx {
     int[] a = new int[] {1, 2, 2, 2, 3, 6, 7, 7, 7, 10, 11, 11, 12, 13};
     int n = a.length;
     // 变体一：查找第一个值等于给定值的元素
-    System.out.println("查找第一个值等于给定值的元素: " + bsearch1(a, n, 7));
+    System.out.println("查找第一个值等于给定值的元素: " + bsearch1(a, n, 1));
     // 变体二：查找最后一个值等于给定值的元素
-    System.out.println("查找最后一个值等于给定值的元素: " + bsearch2(a, n, 2));
+    System.out.println("查找最后一个值等于给定值的元素: " + bsearch2(a, n, 13));
     // 变体三：查找第一个大于等于给定值的元素
     System.out.println("查找第一个大于等于给定值的元素: " + bsearch3(a, n, 2));
     // 变体四：查找最后一个小于等于给定值的元素
@@ -39,7 +39,7 @@ public class BsearchEx {
       int mid = (low + high) / 2;
       if (a[mid] < value) {
         low = mid + 1;
-      } else if (a[mid] > value || (a[mid] == value && a[mid - 1] == value)) {
+      } else if (mid != 0 && (a[mid] > value || (a[mid] == value && a[mid - 1] == value))) {
         high = mid - 1;
       } else {
         return mid;
@@ -58,9 +58,9 @@ public class BsearchEx {
     int high = n - 1;
     while (low <= high) {
       int mid = (low + high) / 2;
-      if (a[mid] < value) {
+      if (mid != n - 1 && (a[mid] < value || (a[mid] == value && a[mid + 1] == value))) {
         low = mid + 1;
-      } else if (a[mid] > value || (a[mid] == value && a[mid + 1] == value)) {
+      } else if (a[mid] > value) {
         high = mid - 1;
       } else {
         return mid;
@@ -75,6 +75,18 @@ public class BsearchEx {
    * @return the int
    */
   public static int bsearch3(int[] a, int n, int value) {
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (a[mid] < value) {
+        low = mid + 1;
+      } else if (mid != 0 && (a[mid] > value || (a[mid] == value && a[mid - 1] >= value))) {
+        high = mid - 1;
+      } else {
+        return mid;
+      }
+    }
     return -1;
   }
 
@@ -84,6 +96,18 @@ public class BsearchEx {
    * @return the int
    */
   public static int bsearch4(int[] a, int n, int value) {
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (mid != n - 1 && (a[mid] < value || (a[mid] == value && a[mid + 1] <= value))) {
+        low = mid + 1;
+      } else if (a[mid] > value) {
+        high = mid - 1;
+      } else {
+        return mid;
+      }
+    }
     return -1;
   }
 }
